f18919dce9b1197e4d6f087f26e1e3bbbdae8b23
imhasibulhasan <hashibul218@gmail.com>
2021-10-02 23:19:05 +0600

    Update shop name & image show in UI

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
@@ -25,6 +25,8 @@ const showProducts = (products) => {
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+// Add to cart & count cart product
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
